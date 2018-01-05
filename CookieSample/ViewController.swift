2be65ac82b2afcda72875118710fe9d256bc09f4
kazuki229 <tsuzuku.k@gmail.com>
2018-01-05 19:16:41 +0900

    initialize

---
+++ CookieSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookieSample
+//
+//  Created by kazuki229 on 2018/01/05.
+//  Copyright © 2018年 kazuki229. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
